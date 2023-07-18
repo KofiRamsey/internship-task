ccbedc19a4ad64963f99002ea632338f2263c262
KofiRamsey <110561560+KofiRamsey@users.noreply.github.com>
2023-07-18 17:35:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import matplotlib.pyplot as plt
-import base64
+import json
 from io import BytesIO
+import base64
+from numpy import int64, float64
 
 app = Flask(__name__)
 
@@ -11,14 +13,25 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
+def get_data_for_month(df, month):
+    month_data = df[df['Month'] == month]
+    if not month_data.empty:
+        income = month_data['Income'].iloc[0]
+        expense = month_data['Expense'].iloc[0]
+        return income, expense
+    else:
+        return None, None
+
+
 def generate_temporal_graph(df):
+    # Create numerical month representations
     month_num = range(1, len(df) + 1)
 
     # Plot the data as a double bar graph
-    plt.figure(figsize=(12, 5))  # Increase the figure size
+    plt.figure(figsize=(13, 5))
     bar_width = 0.4
-    spacing = 0  # Additional spacing between the bars
-    font_size = 6  # Font size for the bar labels
+    spacing = 0
+    font_size = 6
 
     plt.bar([num + spacing for num in month_num], df['Income'], label='Income', color='green', width=bar_width)
     plt.bar([num + bar_width + spacing * 2 for num in month_num], df['Expense'], label='Expense', color='red',
@@ -39,16 +52,31 @@ def generate_temporal_graph(df):
 
     plt.xticks([num + bar_width + spacing for num in month_num], df['Month'], rotation=45)
 
-    # Save the graph as a base64-encoded string
+    # Save the graph to a bytes buffer and convert it to a base64 string
     buffer = BytesIO()
     plt.tight_layout()
     plt.grid(True)
     plt.savefig(buffer, format='png')
     buffer.seek(0)
-    graph_base64 = base64.b64encode(buffer.read()).decode()
+    graph_data = base64.b64encode(buffer.getvalue()).decode()
     plt.close()
 
-    return graph_base64
+    # Calculate highest income and expense months
+    highest_income_month = df.loc[df['Income'].idxmax(), 'Month']
+    highest_expense_month = df.loc[df['Expense'].idxmax(), 'Month']
+
+    # Calculate lowest income and expense months
+    lowest_income_month = df.loc[df['Income'].idxmin(), 'Month']
+    lowest_expense_month = df.loc[df['Expense'].idxmin(), 'Month']
+
+    # Calculate average income and expense for the whole dataset
+    average_income = df['Income'].mean()
+    average_expense = df['Expense'].mean()
+
+    # Get the data for each month as a dictionary
+    months_data = df.to_dict(orient='list')
+
+    return graph_data, highest_income_month, highest_expense_month, lowest_income_month, lowest_expense_month, average_income, average_expense, months_data
 
 
 @app.route('/')
@@ -56,6 +84,7 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/upload', methods=['POST'])
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
@@ -67,11 +96,42 @@ def upload_file():
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(file_path)
 
+            # Process the Excel file
             df = pd.read_excel(file_path)
 
-            graph_data = generate_temporal_graph(df)
+            # Store the DataFrame in the global variable
+            app.config['uploaded_df'] = df
 
-            return render_template('graph.html', graph_data=graph_data)
+            # Generate temporal graph and other data
+            graph_data, highest_income_month, highest_expense_month, lowest_income_month, lowest_expense_month, average_income, average_expense, months_data = generate_temporal_graph(df)
+
+            return render_template('graph.html',
+                                   graph_data=graph_data,
+                                   highest_income_month=highest_income_month,
+                                   highest_expense_month=highest_expense_month,
+                                   lowest_income_month=lowest_income_month,
+                                   lowest_expense_month=lowest_expense_month,
+                                   average_income=average_income,
+                                   average_expense=average_expense,
+                                   months_data=months_data)
+
+
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    if request.method == 'POST':
+        data = request.get_json()
+        selected_month = data.get('month')
+
+        # Get the uploaded DataFrame from the global variable
+        df = app.config['uploaded_df']
+
+        income, expense = get_data_for_month(df, selected_month)
+
+        # Convert income and expense to standard Python data types
+        income = int(income) if isinstance(income, (int64, int)) else float(income)
+        expense = int(expense) if isinstance(expense, (int64, int)) else float(expense)
+
+        return jsonify({'income': income, 'expense': expense})
 
 
 if __name__ == '__main__':
